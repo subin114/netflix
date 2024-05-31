4067d0902e19d8bce039a74f15b5b58b5126e0bd
subin114 <bin7851@naver.com>
2024-05-31 15:38:23 +0900

    UI 수정

--- src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
+++ src/pages/Homepage/components/PopularMovieSlide/PopularMovieSlide.jsx
@@ -22,16 +22,36 @@ const PopularMovieSlide = () => {
 
   /** 반응형 */
   const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 9,
+    desktop1: {
+      breakpoint: { max: 3000, min: 1880 },
+      items: 8,
+    },
+    desktop2: {
+      breakpoint: { max: 1880, min: 1700 },
+      items: 7,
+    },
+    desktop3: {
+      breakpoint: { max: 1700, min: 1550 },
+      items: 6,
+    },
+    desktop4: {
+      breakpoint: { max: 1550, min: 1300 },
+      items: 5,
+    },
+    desktop5: {
+      breakpoint: { max: 1300, min: 1080 },
+      items: 4,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1080, min: 865 },
+      items: 3,
+    },
+    tablet2: {
+      breakpoint: { max: 865, min: 645 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 645, min: 300 },
       items: 1,
     },
   };
